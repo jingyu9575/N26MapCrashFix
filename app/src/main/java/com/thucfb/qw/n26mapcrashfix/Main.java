fb2c99909da56393ff44f69d4cb626b6d85e4f83
jingyu9575 <qw2g64@outlook.com>
2019-11-24 16:02:40 +0100

    Initial commit

---
+++ app/src/main/java/com/thucfb/qw/n26mapcrashfix/Main.java
@@ -0,0 +1,23 @@
+package com.thucfb.qw.n26mapcrashfix;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+
+@SuppressWarnings("unused")
+public class Main implements IXposedHookLoadPackage {
+	@Override
+	public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+		if (!lpparam.packageName.equals("de.number26.android"))
+			return;
+		findAndHookMethod("de.number26.machete.android.ui.map.MapFragment", lpparam.classLoader, "uc", new XC_MethodHook() {
+			@Override
+			protected void afterHookedMethod(MethodHookParam param) {
+				if (param.getThrowable() != null)
+					param.setResult(null);
+			}
+		});
+	}
+}
